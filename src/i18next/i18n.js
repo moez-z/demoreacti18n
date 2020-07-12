dd4aea6775874ea6769e5e5cc50ada5927736ea3
moez-z <moezzermani32@gmail.com>
2020-07-12 15:51:46 +0100

    change  message

--- src/i18next/i18n.js
+++ src/i18next/i18n.js
@@ -2,13 +2,13 @@ import i18n from "i18next"
 const resources ={
     en : {
         translation :{
-            "welcome" : "welcome to react and react-i18next",
+            "welcome" : "welcome to react and react- 1",
             "login" : "login"
         }
     },
     fr : {
         translation :{
-            "welcome" : "bonjour react and react-i18next",
+            "welcome" : "bonjour react and react-i18next 2",
             "login": "connection"
         }
     }
